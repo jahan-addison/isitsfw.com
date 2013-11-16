5c71e349313563d34dc366edadc1ff31166c8f5c
Jahan Addison <jahan.addison@jacata.me>
2013-11-16 16:51:19 -0500

    add env variable and specifiy yui compressor

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'less'
 require 'sinatra/assetpack'
 
 class App < Sinatra::Base
+  set :environment, :development
   set :root, File.dirname(__FILE__)
   set :bind, '0.0.0.0'
   set :port, 8000
@@ -17,19 +18,18 @@ class App < Sinatra::Base
     serve '/css',    from: 'public/css'       # Default
 
     css :main, [
-      '/css/main.less'
+      '/css/main.css'
     ]
 
     # The second parameter defines where the compressed version will be served.
     # (Note: that parameter is optional, AssetPack will figure it out.)
-    js :app, '/js/app.js', [
+    js :main, '/js/main.js', [
       '/js/vendor/**/*.js',
       '/js/lib/**/*.js'
     ]
 
-
-    js_compression  :jsmin    # :jsmin | :yui | :closure | :uglify
-    css_compression :simple   # :simple | :sass | :yui | :sqwish
+    js_compression  :yui  # :jsmin | :yui | :closure | :uglify
+    css_compression :yui  # :simple | :sass | :yui | :sqwish
   }
 
   get '/' do

0aaa40f2545548b67333b28c313f69658909bc90
Jahan Addison <jahan.addison@jacata.me>
2013-11-22 17:39:33 -0500

    starting search animations and user experience

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,10 @@
 $(function() {
 
   /* URI Validation */
-  (function() {
+  (function(window, document) {
+    // expose the URI object
     $('#search').on('keyup blur focusout', function() {
-      var url    = URI($(this).val()),
+        var url  = URI($(this).val()),
           proto  = url._parts.protocol,
           host   = url._parts.domain,
           errors = false;
@@ -19,6 +20,44 @@ $(function() {
         $(this).next().removeClass('error');
       }
     });
-  })();
+  })(window, document);
   /* End of URI Validation */
+
+  /* Search animation */
+  (function() {
+    var spinner = "<div class='pre-loader'></div>";
+    var response = [
+      "<div class='response'>",
+      "",
+      "</div>"
+    ].join();
+    /*
+    var res = function() {
+      $('loading').fadeOut('slow', function() {
+        $('#animate').animate({
+            left: "0"
+         }, 'slow', function() {
+          $('#box').animate({top: '50px'}, function() {
+
+          })
+         });
+      });
+    };
+    */
+    $('#search + button').click(function() {
+      var self = this;
+      var errors = function() {
+        return $('#search').val().length === 0 || $(self).hasClass('error');
+      }();
+      if (!errors) {
+        $('#animate').animate({
+          left: "20px"
+        }, 'slow', function() {
+          $('.loading').html(spinner)
+            .fadeIn('slow');
+        });
+      }
+    });
+  })();
+  /* end of Search animation */
 });

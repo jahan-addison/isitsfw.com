6df18ba612f4dc2537b57d9590b2a0fcb1779475
Jahan Addison <jahan.addison@jacata.me>
2013-11-26 17:12:35 -0500

    preparing for real data and images; lightbox

--- Gemfile
+++ Gemfile
@@ -2,9 +2,14 @@ source 'https://rubygems.org'
 
 ruby '1.9.3'
 
+# platform
 gem 'sinatra'
 gem 'thin'
 
+# deps
+gem 'tesseract-ocr'
+
+# assets
 gem 'less'
 gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
 gem 'yui-compressor',    :require => 'yui/compressor'
--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,95 @@
-/*
-  Can't help but feel they're some bad anti-patterns
-  in this... to fix and optimize soon
- */
 $(function() {
+
+  /* Lightbox function */
+  var $lightbox = function(contents, width, height) {
+    var width  = width  || 600;
+    var height = height || 500;
+    var $html  = $('<div id="lightbox-outer"> <div id="lightbox"><span class="close">x</span></div> </div>');
+   
+    $html.find('#lightbox')
+      .css({
+        left: ($(window).width() - width) / 2,
+        top: (($(window).scrollTop() + ($(window).height())) - height) /2,
+        width: width,
+        height: height
+        })
+      .append(contents);
+
+    $html
+      .insertBefore('#outer-wrapper')
+      .fadeIn('fast')
+      .find('#lightbox')
+        .fadeIn('fast');
+
+    $('.close').on('click', function() {
+      $html.fadeOut('fast', function(){
+        $html.remove();
+      });
+    });
+  };
+  /* end of lightbox function */
+
+  /* Response function */
+  var $response = function() {
+    var responses           = [
+      "<div class='response no'>NO! <a href='#'>(why?)</a></div>",
+      "<div class='response maybe'>MAYBE? <a href='#'>(more options)</a></div>",
+      "<div class='response not-sure'>NOT SURE! <a href='#'>(why?)</a></div>",
+      "<div class='response yes'>YES! <a href='#'>(why?)</a></div>"
+    ];    
+    Array.prototype.inArray = function(needle) {
+      var exists = false;
+      this.forEach(function(e) {
+        if (e === needle) {
+          exists = true;
+        }
+      });
+      return exists;
+    };
+    var images   = ['jpg', 'jpeg', 'png', 'bmp', 'gif', 'tiff'];
+        filename = $uri.filename() || undefined;
+    var ext      = $uri.suffix()   || undefined;
+
+    console.log(ext);
+
+    $('.loading').fadeOut('fast', function() {
+      $('#animate').animate({
+          left: "0"
+       }, 'slow', function() {
+        // images
+        if (ext && images.inArray(ext)) {
+          $lightbox(ext);
+        } 
+        // some other file type
+        else if (ext) {
+          $('#box').animate({top: '57px'}, 455, function() {
+            $(responses[2]).insertBefore('#box');
+          });          
+        }
+        // any thing else
+        else
+        {
+          $('#box').animate({top: '57px'}, 455, function() {
+            $(responses[0]).insertBefore('#box');
+          });
+        }
+       });
+    });    
+  };
+  /* end of Response function */
 
   /* URI Validation */
   (function(window, document) {
-    // expose the URI object
     $('#search').on('keyup blur focusout', function(e) {
         $(this).next().attr('style', '');
         if (e.which === 13) {
           $('#search + button').click();
           return false;
         }
-        var url  = URI($(this).val()),
-          proto  = url._parts.protocol,
-          host   = url._parts.domain,
-          errors = false;
+        var url   = window.$uri = new URI($(this).val());
+        var proto = url._parts.protocol,
+          host    = url._parts.domain,
+          errors  = false;
       if (proto && !proto.match(/^https?$/)) { 
         errors = true; 
       }
@@ -42,7 +115,7 @@ $(function() {
   })(window, document);
   /* End of URI Validation */
 
-  /* Search animation */
+  /* Search */
   (function() {
     var spinner = ['<ul class="spinner">',
       '<li></li>',
@@ -51,42 +124,21 @@ $(function() {
       '<li></li>',
       '</ul>'
     ].join("\n");
-    var response = [
-      "<div class='response no'>NO! <a href='#'>(why?)</a></div>",
-      "<div class='response maybe'>MAYBE? <a href='#'>(why?)</a></div>",
-      "<div class='response not-sure'>NOT SURE! <a href='#'>(why?)</a></div>",
-      "<div class='response yes'>YES! <a href='#'>(why?)</a></div>"
-    ];
-    
-    var res = function() {
-      $('.loading').fadeOut('fast', function() {
-        $('#animate').animate({
-            left: "0"
-         }, 'slow', function() {
-          $('#box').animate({top: '57px'}, 455, function() {
-            $(response[0]).insertBefore('#box');
-          });
-         });
-      });
-    };
-    
     $('#search + button').click(function() {
       var self = this;
       $('#box').animate({top: '0'}, 'slow');
       $('.response').remove();
-      var errors = function() {
-        return $('#search').val().length === 0 || $(self).hasClass('error');
-      }();
+      var errors = $('#search').val().length === 0 || $(self).hasClass('error');
       if (!errors) {
         $('#animate').animate({
           left: "20px"
         }, 455, function() {
           $('.loading').html(spinner)
             .fadeIn(1000);
-          window.setTimeout(res, 7500);
+          window.setTimeout($response, 7500);
         });
       }
     });
   })();
-  /* end of Search animation */
+  /* end of Search */;
 });

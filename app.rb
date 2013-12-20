fd14e2c9c1500064b69431c71a7c0a4c71d77fb5
Jahan Addison <jahan.addison@jacata.me>
2013-12-20 14:33:56 -0500

    rest-client timeout

--- app.rb
+++ app.rb
@@ -192,7 +192,7 @@ EOF
         @image   = Images.first({:image_hash => hash})
         if @image.nil?
           url      = "https://nds.p.mashape.com/?url=" << URI.escape("http://i.embed.ly/1/image/resize?url=" << escaped << "&key=c814a1d73fcc48ccab27c8830d92f26b&width=700&height=600", Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-          response = RestClient::Request.execute(:method => :get, :url => url, :timeout => 25000, :open_timeout => 25000, :headers => {
+          response = RestClient::Request.execute(:method => :get, :url => url, :timeout => 90000000, :open_timeout => 90000000, :headers => {
             "X-Mashape-Authorization" => "oDpSINvANRazu7Yi9772wDrcaeHsYKMN"})
           data = JSON.parse(response.body)
           if data["is_nude"]    == 'true'
